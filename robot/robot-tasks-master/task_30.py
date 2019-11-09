c20efca9e2b4afeff84ecec65540d2d90319070c
Bochkarev90 <bochkarev.rabota@gmail.com>
2019-11-09 21:05:45 +0300

    Complete task 30

--- robot/robot-tasks-master/task_30.py
+++ robot/robot-tasks-master/task_30.py
@@ -5,7 +5,38 @@ from pyrob.api import *
 
 @task(delay=0.01)
 def task_9_3():
-    pass
+    def fill_the_string(first_unfilled_cell_position: int, 
+            second_unfilled_cell_position: int
+            ):
+        """ Fills all cells in the string except cells from parameters.
+            Then move to the first sell of next string if possible
+        """
+        i = 0
+        while True:
+            if i != first_unfilled_cell_position \
+                    and i != second_unfilled_cell_position:
+                fill_cell()
+            if wall_is_on_the_right():
+                break
+            move_right()
+            i += 1
+        while not wall_is_on_the_left():
+            move_left()
+        if not wall_is_beneath():
+            move_down()
+            
+    
+    number_of_cells = 1
+    while not wall_is_on_the_right():
+        move_right()
+        number_of_cells += 1
+    while not wall_is_on_the_left():
+        move_left()
+    
+    for i in range(number_of_cells):
+        fill_the_string(i, number_of_cells-1-i)
+
+
 
 
 if __name__ == '__main__':
