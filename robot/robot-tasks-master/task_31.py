a0dfc91b17e8dc1188c0aee005d4bf21bdd1b0d4
Bochkarev90 <bochkarev.rabota@gmail.com>
2019-11-09 21:12:50 +0300

    Complete task 31

--- robot/robot-tasks-master/task_31.py
+++ robot/robot-tasks-master/task_31.py
@@ -3,9 +3,18 @@
 from pyrob.api import *
 
 
-@task(delay=0.01)
+@task(delay=0.11)
 def task_8_30():
-    pass
+    while True:
+        while not wall_is_on_the_right():
+            move_right()
+        while wall_is_beneath():
+            if wall_is_on_the_left():
+                return
+            move_left()
+        while not wall_is_beneath():
+            move_down()
+
 
 
 if __name__ == '__main__':
