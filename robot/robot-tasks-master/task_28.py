51f31c21ed5c304a1a37ad74d68aded3ca62ab1b
Bochkarev90 <bochkarev.rabota@gmail.com>
2019-11-09 17:47:44 +0300

    Complete task 28

--- robot/robot-tasks-master/task_28.py
+++ robot/robot-tasks-master/task_28.py
@@ -5,7 +5,13 @@ from pyrob.api import *
 
 @task
 def task_7_6():
-    pass
+    i = 0
+    while True:
+        if cell_is_filled():
+            i += 1
+        if i == 5:
+            break
+        move_right()
 
 
 if __name__ == '__main__':
