73bc5d19dada8b70b220532df22747a0f725f5d7
Bochkarev90 <bochkarev.rabota@gmail.com>
2019-11-09 17:14:58 +0300

    Complete task 25

--- robot/robot-tasks-master/task_25.py
+++ robot/robot-tasks-master/task_25.py
@@ -5,7 +5,29 @@ from pyrob.api import *
 
 @task
 def task_2_2():
-    pass
+    def cross():
+        """ Draws a cross
+            Starts and ends in the left top point
+        """
+        move_right()
+        fill_cell()
+        move_down()
+        fill_cell()
+        move_left()
+        fill_cell()
+        move_right(2)
+        fill_cell()
+        move_down()
+        move_left()
+        fill_cell()
+        move_left()
+        move_up(2)
+    
+    move_down()
+    for _ in range(4):
+        cross()
+        move_right(4)
+    cross()
 
 
 if __name__ == '__main__':
