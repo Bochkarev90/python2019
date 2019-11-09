4553d9ff7c374e2cc3848d202664bc6d28737f67
Bochkarev90 <bochkarev.rabota@gmail.com>
2019-11-09 16:58:56 +0300

    Complete task 23

--- robot/robot-tasks-master/task_23.py
+++ robot/robot-tasks-master/task_23.py
@@ -5,7 +5,18 @@ from pyrob.api import *
 
 @task(delay=0.01)
 def task_6_6():
-    pass
+    while True:
+        move_right()
+        while not wall_is_above():
+            move_up()
+        while not wall_is_beneath():
+            fill_cell()
+            move_down()
+        if wall_is_on_the_right():
+            while wall_is_beneath():
+                move_left()
+            break
+
 
 
 if __name__ == '__main__':
