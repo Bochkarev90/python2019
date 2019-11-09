71afe003e9f9906644a1d80943e5782d1c9ac419
Bochkarev90 <bochkarev.rabota@gmail.com>
2019-11-09 14:50:40 +0300

    Complete task 21

--- robot/robot-tasks-master/task_21.py
+++ robot/robot-tasks-master/task_21.py
@@ -5,7 +5,16 @@ from pyrob.api import *
 
 @task(delay=0.05)
 def task_4_11():
-    pass
+    for i in range(13):
+        move_down()
+        move_right()
+        for _ in range(i+1):
+            fill_cell()
+            move_right()
+        while not wall_is_on_the_left():
+            move_left()
+    move_down()
+    move_right()
 
 
 if __name__ == '__main__':
