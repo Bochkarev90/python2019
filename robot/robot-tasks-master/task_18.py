569693e1a303394566889a55f99af6a1d77762ce
Bochkarev90 <bochkarev.rabota@gmail.com>
2019-11-09 14:05:52 +0300

    Complete task 18

--- robot/robot-tasks-master/task_18.py
+++ robot/robot-tasks-master/task_18.py
@@ -5,7 +5,18 @@ from pyrob.api import *
 
 @task
 def task_8_28():
-    pass
+    while not wall_is_on_the_right():
+        move_right()
+    while True:
+        if not wall_is_above():
+            while not wall_is_above():
+                move_up()
+            while not wall_is_on_the_left():
+                move_left()
+            break
+        else:
+            move_left()
+        
 
 
 if __name__ == '__main__':
