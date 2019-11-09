a0a5d4b26b9af4fedfc8b912146efbcbe6710932
Bochkarev90 <bochkarev.rabota@gmail.com>
2019-11-09 17:53:10 +0300

    Complete task 29

--- robot/robot-tasks-master/task_29.py
+++ robot/robot-tasks-master/task_29.py
@@ -5,7 +5,17 @@ from pyrob.api import *
 
 @task
 def task_7_7():
-    pass
+    i = 0
+    while True:
+        if cell_is_filled():
+            i += 1
+        else:
+            i = 0
+        if i == 3:
+            break
+        if wall_is_on_the_right():
+            break
+        move_right()
 
 
 if __name__ == '__main__':
