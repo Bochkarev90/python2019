b4f0383ac7f57cb3ca145f05dd45558fceafc688
Bochkarev90 <bochkarev.rabota@gmail.com>
2019-11-10 23:43:34 +0300

    Complete task 5

---
+++ turtle/task_5.py
@@ -0,0 +1,23 @@
+import turtle
+
+def draw_square(n):
+    """ n - side length
+    """
+    for _ in range(4):
+        turtle.forward(n)
+        turtle.left(90)
+
+def draw_squares(n, distance):
+    """ n - number of squares
+        distance - distance between squares
+    """
+    for i in range(n):
+        turtle.penup()
+        turtle.backward(distance)
+        turtle.right(90)
+        turtle.forward(distance)
+        turtle.left(90)
+        turtle.pendown()
+        draw_square(i * distance * 2)
+
+draw_squares(10, 10)
